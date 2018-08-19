ab3bdd06b90729aaf6595840d97d0b454d6ba7f3
shikdernyc <shikdernyc@gmail.com>
2018-08-19 00:36:48 +0000

    Changed button margins

--- src/Options.js
+++ src/Options.js
@@ -16,9 +16,9 @@ class Options extends Component{
     render(){
         return (
             <div className="row my-5">
-                <button type="button" className="btn btn-secondary col mx-5" onClick={this.addPlayer}>Add Player</button>
-                <button type="button" className="btn btn-secondary col mx-5" onClick={this.reset}>Reset</button>
-                <button type="button" className="btn btn-secondary col mx-5" onClick={this.startingCash}>Starting Cash</button>
+                <button type="button" className="btn btn-secondary col mx-3" onClick={this.addPlayer}>Add Player</button>
+                <button type="button" className="btn btn-secondary col mx-3" onClick={this.reset}>Reset</button>
+                <button type="button" className="btn btn-secondary col mx-3" onClick={this.startingCash}>Starting Cash</button>
             </div>
         )
     }
