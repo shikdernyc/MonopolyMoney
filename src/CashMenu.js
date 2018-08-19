98f48ef40d4365e2c31553f396ca8ef8e4a0089a
shikdernyc <shikdernyc@gmail.com>
2018-08-19 00:35:52 +0000

    Created stylistic changes to get items aligned

--- src/CashMenu.js
+++ src/CashMenu.js
@@ -1,8 +1,8 @@
 import React, {Component} from 'react'
 
 class CashMenu extends Component{
-    constructor(){
-        
+    constructor(props){
+        super(props)
     }
     
     handleAmount(event){
@@ -27,30 +27,29 @@ class CashMenu extends Component{
     }
     
     render(){
-        let {players} = this.props
+        // let {players} = this.props
         
         return(
-            <div>
-                <input type="text" class="form-control" id="amount" onChange={this.handleAmount} placeholder="$500"/>
+            <div className="container">
                 <div className="row">
-                    <button type="button" className="btn btn-secondary col mx-5" onClick={this.handleAddAmount}>Add</button>
-                    <button type="button" className="btn btn-secondary col mx-5" onClick={this.reset}>Remove</button>
+                    <input type="text" className="form-control col mx-2 my-2" id="amount" onChange={this.handleAmount} placeholder="$500"/>
                 </div>
                 <div className="row">
-                  <div class="form-group col">
-                    <label>Players</label>
-                    <select class="form-control">
+                    <button type="button" className="btn btn-secondary col mx-2 my-2" onClick={this.handleAddAmount}>Add</button>
+                    <button type="button" className="btn btn-secondary col mx-2 my-2" onClick={this.reset}>Remove</button>
+                </div>
+                <div className="row">
+                    <select class="form-control col mx-2 my-2">
                       <option>Player 1</option>
                       <option>Player 2</option>
                       <option>Player 3</option>
                       <option>Player 4</option>
                       <option>Player 5</option>
                     </select>
-                  </div>                    
-                  <button type="button" className="btn btn-secondary col mx-5" onClick={this.handleTransferAmount}>Send</button>
+                    <button type="button" className="btn btn-secondary col mx-2 my-2" onClick={this.handleTransferAmount}>Transfer</button>
                 </div>
                 <div className="row">
-                    <button type="button" className="btn btn-secondary col mx-5" onClick={this.giveSalary}>Give Salary</button>
+                    <button type="button" className="btn btn-secondary col mx-2 my-2" onClick={this.giveSalary}>Give Salary</button>
                 </div>
             </div>
         )
