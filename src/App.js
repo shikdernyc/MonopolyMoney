2bd362bc36b628fbe84f0c968f0009f184ba215a
shikdernyc <shikdernyc@gmail.com>
2018-08-19 01:41:10 +0000

    created an addPlayer option which adds a new player to state

--- src/App.js
+++ src/App.js
@@ -6,12 +6,23 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      startCash: 1500,
+      startingBalance: 1500,
       salary: 200,
-      players: {
-        // <Playername>: <PlayerObject> format
-      }
+      players: [
+          // Stored in a {name: name, balance: balance} format
+        ]
     }
+    this.addPlayer = this.addPlayer.bind(this)
+  }
+  
+  addPlayer(playerName){
+    const newPlayer = {
+      name: playerName,
+      balance: this.state.startingBalance
+    }
+    let currentPlayers = this.state.players.slice()
+    currentPlayers.push(newPlayer)
+    this.setState({players: currentPlayers})
   }
   
   render() {
@@ -19,7 +30,7 @@ class App extends Component {
       <div className="App">
         <div className="container">
           <h1 className="text-center display-3">Monopoly Money</h1>
-          <Options />
+          <Options startingBalance={this.state.startingBalance} addPlayer={this.addPlayer}/>
           <Players />
         </div>
       </div>
