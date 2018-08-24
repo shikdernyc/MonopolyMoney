039d41002fdafdc41ddb004c5c1e93feee6180e2
shikdernyc <shikdernyc@gmail.com>
2018-08-24 19:23:47 -0400

    created a setPlayerBalance method to set different player's balance from state

--- src/App.js
+++ src/App.js
@@ -24,8 +24,18 @@ class App extends Component {
     this.state = this.defaultState
     this.addPlayer = this.addPlayer.bind(this)
     this.reset = this.reset.bind(this)
+    this.setPlayerBalance = this.setPlayerBalance.bind(this)
   }
-  
+
+  setPlayerBalance(playerName, newBalance){
+    let newPlayers = this.state.players.slice()
+    let player = newPlayers.find(function(player){return player.name===playerName})
+    player.balance = newBalance
+    console.log(player)
+    this.setState({players: newPlayers})
+  }
+
+  // Options
   reset(){
     this.setState(this.defaultState)
   }
@@ -46,7 +56,7 @@ class App extends Component {
         <div className="container">
           <h1 className="text-center display-3">Monopoly Money</h1>
           <Options startingBalance={this.state.startingBalance} addPlayer={this.addPlayer} reset={this.reset}/>
-          <Players players={this.state.players} />
+          <Players players={this.state.players} setPlayerBalance={this.setPlayerBalance}/>
         </div>
       </div>
     );
